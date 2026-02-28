3de11552bb199046c72eb6090493b67a4fa1c795
pippaJeanne <yhranlo@gmail.com>
2026-02-28 16:10:22 -0500

    trying to create blob store

--- src/routes/api/logChoices/+server.ts
+++ src/routes/api/logChoices/+server.ts
@@ -1,5 +1,6 @@
 import { getStore } from '@netlify/blobs';
 //import type { Context } from "@netlify/functions";
+export const prerender = false;
 export const config = {
   runtime: "nodejs"
 };
@@ -10,6 +11,6 @@ export async function POST({ request }) {
 
   const key = `${Date.now()}-${crypto.randomUUID()}`;
   await store.set(key, JSON.stringify(data));
-
+console.log(key, data)
   return new Response('ok');
 }
