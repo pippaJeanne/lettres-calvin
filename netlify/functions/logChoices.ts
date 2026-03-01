eeba140066d09e07bf65be1c898d7ae0801bb373
pippaJeanne <yhranlo@gmail.com>
2026-03-01 00:48:59 -0500

    blob store created --deleting credentials

--- netlify/functions/logChoices.ts
+++ netlify/functions/logChoices.ts
@@ -1,20 +1,13 @@
 import { getStore } from '@netlify/blobs';
 import type { Handler } from "@netlify/functions"
 
-console.log("SITE ID:", process.env.NETLIFY_SITE_ID);
-console.log("BLOBS TOKEN:", process.env.NETLIFY_BLOBS_TOKEN);
-
 export const handler:Handler = async (event) => {
   const data = JSON.parse(event.body || "{}");
-  const store = getStore({
-    name: "pdf-choices",
-    siteID: process.env.MY_SITE_ID!,
-    token: process.env.MY_BLOBS_TOKEN!,
-  });
+  const store = getStore("pdf-choices");
 
   const key = `${Date.now()}-${crypto.randomUUID()}`;
   await store.set(key, JSON.stringify(data));
-console.log(key, data)
+ console.log(key, data)
   return {
     statusCode:200,
     body: 'ok'
