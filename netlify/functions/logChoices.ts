91a5abf6d83546c6823908e9ee345524db127b8e
pippaJeanne <yhranlo@gmail.com>
2026-03-01 00:16:46 -0500

    tokens for blobs --fingers crossed

--- netlify/functions/logChoices.ts
+++ netlify/functions/logChoices.ts
@@ -6,7 +6,11 @@ console.log("BLOBS TOKEN:", process.env.NETLIFY_BLOBS_TOKEN);
 
 export const handler:Handler = async (event) => {
   const data = JSON.parse(event.body || "{}");
-  const store = getStore('pdf-choices');
+  const store = getStore({
+    name: "pdf-choices",
+    siteID: process.env.MY_SITE_ID!,
+    token: process.env.MY_BLOBS_TOKEN!,
+  });
 
   const key = `${Date.now()}-${crypto.randomUUID()}`;
   await store.set(key, JSON.stringify(data));
