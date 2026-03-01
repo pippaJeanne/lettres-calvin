44c1a5e7d4c0e38b1a6d3e254f1c306e2190105d
pippaJeanne <yhranlo@gmail.com>
2026-03-01 02:44:29 -0500

    testing integrated access 4 blobs

--- netlify/functions/logChoices.ts
+++ netlify/functions/logChoices.ts
@@ -1,12 +1,15 @@
-import { getStore } from '@netlify/blobs';
+import { getStore, connectLambda } from '@netlify/blobs';
 import type { Handler } from "@netlify/functions"
 
-console.log('node: ', process.version,
+
+export const handler:Handler = async (event, context) => {
+  // 1. Manually bridge the Lambda environment
+  //connectLambda(event); 
+  console.log('node: ', process.version,
       'aws:', process.env.AWS_EXECUTION_ENV ?? "no aws exec env",
       'lambda:', process.env.LAMBDA_TASK_ROOT ?? "no lambda root",
       'blobs:', process.env.NETLIFY_BLOBS_CONTEXT ?? "no blobs context")
       
-export const handler:Handler = async (event) => {
   const data = JSON.parse(event.body || "{}");
   const store = getStore("pdf-choices");
 
